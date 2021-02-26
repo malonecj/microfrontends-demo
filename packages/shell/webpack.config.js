0ad789cd8b1c2ffa110db89807c9831b41fabcf3
Conor Malone <malonecj@gmail.com>
2021-02-26 23:18:28 +0000

    fix webpack config

--- packages/shell/webpack.config.js
+++ packages/shell/webpack.config.js
@@ -3,93 +3,95 @@ const ModuleFederationPlugin = require("webpack").container
   .ModuleFederationPlugin;
 const path = require("path");
 const deps = require("./package.json").dependencies;
-module.exports = (_, argv) => ({
-  entry: "./src/index",
-  mode: "development",
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 3000,
-    historyApiFallback: true,
-    hot: false,
-    hotOnly: false,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, content-type, Authorization",
+module.exports = (_, argv) => {
+  console.log(argv)
+  return {
+    entry: "./src/index",
+    mode: "development",
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      port: 3000,
+      historyApiFallback: true,
+      hot: false,
+      hotOnly: false,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "X-Requested-With, content-type, Authorization",
+      },
     },
-  },
-  resolve: {
-    extensions: [".js", ".mjs", ".jsx", ".css"],
-    alias: {
-      events: "events",
+    resolve: {
+      extensions: [".js", ".mjs", ".jsx", ".css"],
+      alias: {
+        events: "events",
+      },
     },
-  },
-  output: {
-    publicPath: argv.mode === "development"
-      ? "auto"
-      : "https://microfrontends-demo-shell.vercel.app/",
-    chunkFilename: "[id].[contenthash].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: ["@babel/preset-react"],
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.png/,
-        type: 'asset/resource'
-      },
-    ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "shell",
-      filename: "remoteEntry.js",
-      remotes: {
-        home: "home@http://localhost:3001/remoteEntry.js",
-        shell: "shell@http://localhost:3000/remoteEntry.js",
-        searchResults: "searchResults@http://localhost:3002/remoteEntry.js",
-        viewItemPage: "viewItemPage@http://localhost:3003/remoteEntry.js",
-      },
-      exposes: {
-        "./Shell": "./src/Shell",
-        "./Service": "./src/Service",
-      },
-      shared: [
+    output: {
+      publicPath: argv.mode === "production"
+        ? "https://microfrontends-demo.vercel.app/" : "auto",
+      chunkFilename: "[id].[contenthash].js",
+    },
+    module: {
+      rules: [
         {
-          ...deps,
-          react: {
-            singleton: true,
-            requiredVersion: deps.react,
-          },
-          "react-dom": {
-            singleton: true,
-            requiredVersion: deps["react-dom"],
+          test: /\.m?js$/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
           },
         },
-        // Workaround explaination: https://www.youtube.com/watch?v=-LNcpralkjM&t=540
-        "./src/Service",
+        {
+          test: /\.jsx?$/,
+          loader: "babel-loader",
+          exclude: /node_modules/,
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.png/,
+          type: 'asset/resource'
+        },
       ],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-});
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "shell",
+        filename: "remoteEntry.js",
+        remotes: {
+          home: "home@http://localhost:3001/remoteEntry.js",
+          shell: "shell@http://localhost:3000/remoteEntry.js",
+          searchResults: "searchResults@http://localhost:3002/remoteEntry.js",
+          viewItemPage: "viewItemPage@http://localhost:3003/remoteEntry.js",
+        },
+        exposes: {
+          "./Shell": "./src/Shell",
+          "./Service": "./src/Service",
+        },
+        shared: [
+          {
+            ...deps,
+            react: {
+              singleton: true,
+              requiredVersion: deps.react,
+            },
+            "react-dom": {
+              singleton: true,
+              requiredVersion: deps["react-dom"],
+            },
+          },
+          // Workaround explaination: https://www.youtube.com/watch?v=-LNcpralkjM&t=540
+          "./src/Service",
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+    ],
+  }
+};
