68450e88cb177e53791a724f05fbd62e205a863e
Conor Malone <malonecj@gmail.com>
2021-02-26 23:08:14 +0000

    remove service context from home

--- packages/home/src/HomepageService.js
+++ packages/home/src/HomepageService.js
@@ -1,13 +1,7 @@
 import React from "react";
-import { useServiceContext } from "shell/Service";
 import Content from './Content';
 
 export default function Homepage() {
-  const serviceContext = useServiceContext();
-  React.useEffect(() => {
-    serviceContext.setService({ title: "Homepage" });
-  }, []);
-
   return (
     <main>
       <Content />
